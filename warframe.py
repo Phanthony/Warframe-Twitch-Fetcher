94dbc732fa95db145edd402c42b2fa096e830986
Phanthony <33107041+Phanthony@users.noreply.github.com>
2017-10-25 19:14:53 -0500

    Update warframe.py

--- warframe.py
+++ warframe.py
@@ -5,22 +5,23 @@ from subprocess import Popen
 import tkinter
 from tkinter import *
 
-client = TwitchClient(client_id='2l7nb587j3r4r5ewwuyjq34vocq03r')
-streams = client.streams.get_live_streams(game='warframe',limit=100)
+client = TwitchClient(client_id=)#Need to grab twitch client ID
+streams = client.streams.get_live_streams(game='warframe',limit=100)#Change this number to change the amount of streamers the twitch API pulls
 checkList = ['!drop' , 'achievement']
 
 def multiTwitch(myList):
-    Popen('taskkill /F /IM chrome.exe', shell=True)
+    Popen('taskkill /F /IM chrome.exe', shell=True)#Can change "chrome" to any other browser, currently script opens up your defualt browser
+    #Note this will close all instances of what ever browser is put 
     window.after(2000)
-    #multitwitch doesn't allow for more than 20 streams open at once
+    #multitwitch doesn't allow for more than 25 streams open at once
+    #Set my limit to 20 to reduce cpu usage for each multitwitch tab
     while len(myList) > 20:
         url = 'http://multitwitch.tv/'
         for x in range(20):
             url = url + (myList[0] + '/')
             myList.pop(0)
         webbrowser.open_new(url)
-        window.after(120000)
-        #print(url)
+        window.after(120000)#number in ms, currently set to 2 minute intervals between opening each multitwitch tab to allow streams to load
     url = 'http://multitwitch.tv/'
     for streamers in myList:
         url = url + (streamers + '/')
@@ -36,7 +37,7 @@ def warframe():
         if any(x in tester for x in checkList):
             if (streams[counter]['stream_type']) == 'live':
                 if (streams[counter]['channel']['partner']) == False:
-                    if (streams[counter]['viewers']) >= 15:
+                    if (streams[counter]['viewers']) >= 15: #Number of viewers stream must have to be opened up if they aren't partnered
                         streamerID.append(streams[counter]['channel']['name'])
                 else:
                     streamerID.append(streams[counter]['channel']['name'])
@@ -45,22 +46,23 @@ def warframe():
     return ("Finished")
 
 def Drops():
-    global test
+    global repeater
     global startButton
     global stopButton
     startButton.config(state='disabled')
     stopButton.config(state='normal')
     warframe()
-    test = window.after(12000000, Drops)
-    test
+    repeater = window.after(12000000, Drops)#Currently set to 20 minute intervals before it checks for new streamers
+    #This allows offline streams to be closed and then opens up new streams
+    repeater
         
 def End():
     global startButton
     global stopButton    
-    global test
+    global repeater
     startButton.config(state='normal')
     stopButton.config(state='disabled')    
-    window.after_cancel(test)
+    window.after_cancel(repeater)
     Popen('taskkill /F /IM chrome.exe', shell=True)
         
 
